207d33104c532ae7319c52e9e17ecd74138f6fa4
ProfSanjay98 <167111312+ProfSanjay98@users.noreply.github.com>
2024-05-25 02:46:51 -0400

    Start logic for Monster.java

--- Monster.java
+++ Monster.java
@@ -1,3 +1,25 @@
 public class Monster {
-    // Extends Character class?
+	private String name;
+	private String description; 	// For compendium obj
+	private int floor; 	// Floor monster is available on --> might make array for multiple floors
+	private Stats stats; // Initialized in each constructor??
+	private Resources resources; 	// Initialized in constructor
+	// Map for loot drops --> Item & double (percent chance of giving loot)
+	// Skill objects for rune drop?
+	
+	// Constructors?? Or somehow reference a dictionary of Monsters?
+
+	public Item dropLoot() {	// TODO: Lookup functions and figure out logic
+		// Create random number
+		// Check it against library --> whichever has the closest value?
+		return loot;
+	}
+
+	public boolean giveEXP(Compendium playerCompendium) {
+		// Check Compendium obj for entry --> loop through until found
+		// return found ? true : false
+	}
+
+	
+
 }
